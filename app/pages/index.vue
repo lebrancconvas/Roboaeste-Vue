b7da18384c3b019d810f1275e39e1f868baad473
lebrancconvas <lebrancconvas@gmail.com>
2022-12-28 22:26:52 +0700

    ✨ feat: create input section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,13 +5,34 @@
         <h1>Roboaeste</h1>
       </div>
     </header>
+    <section id="input-section">
+      <div>
+        <Input v-model="prompt" placeholder="Insert your prompt..." style="width: 500px" />
+      </div>
+      <div>
+        <Button type="success" @click="genImage">Generated</Button>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-
+  export default {
+    data() {
+      return {
+        prompt: ""
+      }
+    },
+    methods: {
+      genImage() {
+        console.log(this.prompt);
+      }
+    }
+  }
 </script>
 
 <style scoped>
-
+  * {
+    text-align: center;
+  }
 </style>
